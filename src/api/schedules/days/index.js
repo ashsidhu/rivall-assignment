69e43befc8be4bc2349ebbd393e5b7668fe89c83
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2018-04-10 23:49:12 +0530

    Updates dates in games being updated

--- src/api/schedules/days/index.js
+++ src/api/schedules/days/index.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import Promise from "bluebird";
+import moment from "moment";
 import Joi from "joi";
 import { NotFoundError, BadRequestError, ServerError } from "../../../errors";
 import {
   showDays,
   updateGameDay,
   deleteDay,
-  showGamesByDayId,
   getFullSchedule
 } from "../../../models";
 import { asyncMiddleware, validateParams } from "../../../utils/router";
@@ -14,9 +14,24 @@ import { asyncMiddleware, validateParams } from "../../../utils/router";
 const api = new Router();
 
 const mergeSchema = Joi.object().keys({
-  merge_from: Joi.number().integer().min(1).required(),
-  merge_to: Joi.number().integer().min(1).required(),
+  merge_from: Joi.number()
+    .integer()
+    .min(1)
+    .required(),
+  merge_to: Joi.number()
+    .integer()
+    .min(1)
+    .required()
 });
+
+function determineMaxSimultaneousGames(games) {
+  // extract all timestamps, start and end
+  // maintain max counter = 0
+  // sort all timestamps
+  // loop over timestamps
+    // check how many are in each interval
+    // update max counter
+}
 
 api.put(
   "/merge",
@@ -27,7 +42,9 @@ api.put(
     const days = await showDays([merge_to, merge_from]);
     if (days.length < 2) return next(new NotFoundError());
 
-    await updateGameDay(merge_from, merge_to);
+    let toDate = days.filter(d => d.id === merge_to)[0].date;
+    toDate = moment(toDate).format("MM-DD-YYYY");
+    await updateGameDay(merge_from, merge_to, toDate);
     await deleteDay(merge_from);
 
     const scheduleId = days[0].schedule_id;
--- src/models/index.js
+++ src/models/index.js
@@ -7,11 +7,18 @@ export const showDays = async function(dayIds) {
   return await query;
 };
 
-export const updateGameDay = async function(mergeFrom, mergeTo) {
+const timeQuery = field => (`EXTRACT(HOUR FROM ${field}) * INTERVAL '1 HOUR' +
+             EXTRACT(MINUTE FROM ${field}) * INTERVAL '1 MINUTE' +
+             EXTRACT(SECOND FROM ${field}) * INTERVAL '1 SECOND'`)
+
+
+export const updateGameDay = async function(mergeFrom, mergeTo, toDate) {
   const query = db("league_game")
     .where({ day_id: mergeFrom })
     .update({
-      day_id: mergeTo
+      day_id: mergeTo,
+      start_time: db.raw(`'${toDate}'::TIMESTAMP + ${timeQuery("start_time")}`),
+      end_time: db.raw(`'${toDate}'::TIMESTAMP + ${timeQuery("end_time")}`)
     });
   return await query;
 };
@@ -23,10 +30,10 @@ export const deleteDay = async function(id) {
   return await query;
 };
 
-export const showGamesByDayId = async function(dayId) {
+export const showGamesByDayIds = async function(dayIds) {
   const query = db("league_game")
     .select()
-    .where("day_id", dayId);
+    .whereIn("day_id", dayIds);
   return await query;
 };
 
